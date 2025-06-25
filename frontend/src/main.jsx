4577c4fcd094a96f5fe2afacabae4f46a4d0bd37
Sanjay ramesh <sanjayrameshi.23aim@kongu.edu>
2025-06-25 15:46:52 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-// src/main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { UserProvider } from "./context/UserContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { UserProvider } from './context/UserContext';  // ✅ make sure this is the correct path
 import './index.css';
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <UserProvider>
-    <App />
-  </UserProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </React.StrictMode>
 );
