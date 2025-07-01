55ff5258d4e89db9ddac719452f5fafb331e4fcb
Sanjay ramesh <sanjayrameshi.23aim@kongu.edu>
2025-07-01 15:47:05 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://finverse-6juf.onrender.com",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://finverse-6juf.onrender.com",
   withCredentials: true,
 });
 
