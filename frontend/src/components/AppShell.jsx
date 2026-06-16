1bcb0398911c28f6014d782193b31e98ff0d5543
Sanjay Ramesh <sanjayking375@gmail.com>
2026-06-16 15:44:08 +0530

    UI updates and minor fixes

--- frontend/src/components/AppShell.jsx
+++ frontend/src/components/AppShell.jsx
@@ -1,19 +1,16 @@
 import Sidebar from "./Sidebar";
 import BottomNavbar from "./BottomNavbar";
-import PullToRefresh from "./PullToRefresh";
 
 export default function AppShell({ children }) {
     return (
-        <PullToRefresh>
-            <div className="flex min-h-screen">
-                <Sidebar />
-                <main className="flex-1 md:ml-16 pb-[calc(5rem+env(safe-area-inset-bottom))] md:pb-0 min-h-screen">
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                        {children}
-                    </div>
-                </main>
-                <BottomNavbar />
-            </div>
-        </PullToRefresh>
+        <div className="flex min-h-screen">
+            <Sidebar />
+            <main className="flex-1 md:ml-16 pb-[calc(5rem+env(safe-area-inset-bottom))] md:pb-0 min-h-screen">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                    {children}
+                </div>
+            </main>
+            <BottomNavbar />
+        </div>
     );
 }
